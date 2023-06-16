216f38a73c1c448d2381fc2a255b1386d14e0a59
dongmyoungLee <pajang1515@daum.net>
2023-06-17 02:21:31 +0900

    Wavve 메인 화면 및 영화 상세화면 작업완료본..

---
+++ src/main/webapp/js/main.js
@@ -0,0 +1,38 @@
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 200,
+    modifier: 3,
+    slideShadows: true
+  },
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  slidesPerView: 2,
+  // autoplay: {
+  //   delay: 3000,
+  //   disableOnInteraction: false
+  // }
+});
+
+var swiper2 = new Swiper(".mySwiper", {
+  slidesPerView: 5,
+  spaceBetween: 15,
+  slidesPerGroup: 5,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
