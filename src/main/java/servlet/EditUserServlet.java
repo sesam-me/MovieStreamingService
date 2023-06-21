96fd6256f62150bb33604c11cde0315cdd541548
ajtwu1235 <ajtwu1235@gmail.com>
2023-06-21 16:19:04 +0900

    회원정보 업데이트 로직 개발

--- src/main/java/servlet/EditUserServlet.java
+++ src/main/java/servlet/EditUserServlet.java
@@ -19,6 +19,8 @@ public class EditUserServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        req.setCharacterEncoding("UTF-8");
         String userName = req.getParameter("userName");
         String email = req.getParameter("email");
         String password = req.getParameter("password");
